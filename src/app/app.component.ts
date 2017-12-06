dbea65ece80ce595170e0e609027442f69933f85
Flor <flor.26071994@gmail.com>
2017-12-06 01:01:12 -0500

    feat(sentence): add array of sentences and show on UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  gridText = [
+      {gamer:'user', sentence: 'Hoy sentí el tufo hediondo'},
+      {gamer:'user', sentence: 'Que psicólogo ni que mierda'},
+      {gamer:'user', sentence: 'Mejor necesito un porro'},
+      {gamer:'user', sentence: 'Un petardo y listo'}
+      {gamer:'user', sentence:'De plano saben que huelen a que se cagan'},
+      {gamer:'user', sentence:'Asco me da como les hieden las patas'},
+      {gamer:'user', sentence:'Solo pelándose el huevo se la pasan'},
+      {gamer:'user', sentence:'Ni qué guapa se fuera a ver la puta'},
+      {gamer:'user', sentence:'Y creen que están realizadas'},
+      {gamer:'user', sentence:'Se vuelve una jodida cadena'},
+      {gamer:'user', sentence:'Una seca cara de mosco'},
+      {gamer:'user', sentence:'Otra que está a mi par que trompa de chupa' },
+      {gamer:'user', sentence:'Hasta ni quiero regresar el 'lunes'' },
+      {gamer:'user', sentence:'Yo pego el mío limpiecito' },
+      {gamer:'user', sentence:'Que buen pedo' },
+      {gamer:'user', sentence:'Quería meterse a un negocio sucio'},
+      {gamer:'user', sentence:'Que bussiness ni que mierda'},
+      {gamer:'user', sentence:'Me huele a huevo podrido'},
+      {gamer:'user', sentence:'A veces pienso que vivo el resultado de mi descuido'},
+      {gamer:'user', sentence:'Porque era un pato'},
+      {gamer:'user', sentence:'Y ahora tal vez sea un tronco vendido'},
+      {gamer:'user', sentence:'Tal vez con la mente les damos vida'},
+
+];
+  
 }
