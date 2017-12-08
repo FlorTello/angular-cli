bbb012f6c83749a2620162a33bba494fe7394692
FlorTello <flor.26071994@gmail.com>
2017-12-07 19:29:55 -0500

    add directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+  counter: number;
+
+
+  constructor() {
+    this.counter = 4;
+  }
+
+  increment(): number {
+    this.
+  }
 }
