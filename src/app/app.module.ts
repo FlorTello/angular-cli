1ec5f27083b6d09710ab775f19701d88d9c5b103
Flor Tello <flor.tello_amable@scotiabank.com.pe>
2017-12-14 15:08:00 -0500

    fix(user): delete dead

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,6 @@ import {MatButtonModule, MatFormFieldModule, MatSnackBarModule} from '@angular/m
     FlexLayoutModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule,
     MatFormFieldModule,
     MatSnackBarModule,
     MatButtonModule
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,28 +1,22 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 
 @Injectable()
 export class UserService {
-  API = 'API';
   data = [];
 
-  constructor(private http: HttpClient) {
+  constructor() {
   }
 
   getSencentes(): Observable<any> {
     return Observable.of(this.data);
-    // return this.http.post(`${this.API}/user`, {
-    //   sentences: sentence
-    // });
   }
 
   setSentence(text: string): Observable<any> {
     this.data.push({
       gamer: 'user', sentence: text
     });
-    console.log(this.data);
     return Observable.of({message: 'success'});
   }
 
